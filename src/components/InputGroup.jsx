ca3508bceb3f5ca605abb2d847b87c6f189ab8a4
LasithEranga <lasitheranga1@gmail.com>
2022-10-13 02:24:59 +0530

    feat: registration page layout

---
+++ src/components/InputGroup.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function InputGroup({ id, type, label }) {
+  return (
+    <div className="mt-2">
+      <label for={id} className="mb-1">
+        {label}
+      </label>
+      <input type={type} className="w-100 form-control" id={id} />
+    </div>
+  );
+}
+
+export default InputGroup;
--- src/pages/Registration/Registration.jsx
+++ src/pages/Registration/Registration.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { Form } from "react-bootstrap";
+import InputGroup from "../../components/InputGroup";
 
 function Registration() {
-  return <div>Registration</div>;
+  return (
+    <div className="vh-100 d-flex justify-content-center align-items-center">
+      <div>image</div>
+      <div>
+        <h4>Enter Your Details</h4>
+        <div className="d-flex gap-2 ">
+          <InputGroup label={"First name"} id="firstName" type="text" />
+          <InputGroup label={"Last name"} id="firstName" type="text" />
+        </div>
+
+        <InputGroup label={"Email"} id="firstName" type="text" />
+        <InputGroup label={"Username"} id="firstName" type="text" />
+        <InputGroup label={"Password"} id="firstName" type="password" />
+        <InputGroup label={"Confirm Password"} id="firstName" type="password" />
+      </div>
+    </div>
+  );
 }
 
 export default Registration;
