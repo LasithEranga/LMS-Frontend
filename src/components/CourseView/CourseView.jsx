d800912fd8e5e168d444637525228b8178ad6832
Kavinda Nimesha <kavindanimesha@gmail.com>
2022-10-15 22:03:09 +0530

    CourseView

---
+++ src/components/CourseView/CourseView.jsx
@@ -0,0 +1,48 @@
+import './CourseView.css';
+
+function CourseView() {
+  return (
+  
+    <div>
+      <table>
+        <tr>
+          <td class = "search"> Search Here: </td>
+          <td class = "search"> Information Communcation And Technologies </td>
+        </tr>
+
+        
+        <div class="grid-container">
+            <div class="item1"></div>
+            <div class="item2">
+              <h2> Software Engineering Concepts </h2>
+              <p> Semester 1 , Year 3 | DR.Dilani Wickramaarachchi</p>
+              <button class = "btn">Enroll</button>
+            </div>
+         </div>
+
+         <div class="grid-container">
+            <div class="item1"></div>
+            <div class="item2">
+              <h2> Software Engineering Concepts </h2>
+              <p> Semester 1 , Year 3 | DR.Dilani Wickramaarachchi</p>
+              <button class = "btn">Enroll</button>
+            </div>
+         </div>
+
+         <div class="grid-container">
+            <div class="item1"></div>
+            <div class="item2">
+              <h2> Software Engineering Concepts </h2>
+              <p> Semester 1 , Year 3 | DR.Dilani Wickramaarachchi</p>
+              <button class = "btn">Enroll</button>
+            </div>
+         </div>
+
+      </table>
+    </div>
+
+    
+  );
+}
+
+export default CourseView;
