8bd59e45aaa245f6d41d3a41681bc18b8892c1ee
kavindagayashan <kava.gayashan@gmail.com>
2022-10-15 10:10:05 +0530

    grades

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Registration from "./pages/Registration/Registration";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./app/store";
+import GradeView from "./pages/GradeView/GradeView";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <BrowserRouter>
           <Routes>
             <Route path="/sign-up" element={<Registration />} />
+            <Route path="/grades" element={<GradeView />} />
           </Routes>
         </BrowserRouter>
       </div>
