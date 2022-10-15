e9df27c98c188fab5c19ab0755557f4eb1cfcf88
kavindagayashan <kava.gayashan@gmail.com>
2022-10-16 00:14:53 +0530

    Icon

--- src/pages/GradeView/GradeView.jsx
+++ src/pages/GradeView/GradeView.jsx
@@ -7,14 +7,13 @@ function GradeView() {
         <h3>Course name</h3>
 
 <div class="row">
-  <div class="d-flex p-4 bg-secondary">
+  <div class="d-flex p-3 bg-secondary">
     <div class ="col-8">INTE 22222 - Management of Technology</div>
-    <div className="text-center col">Some text..</div>
     <div className="text-center col">Some text..</div>
   </div>
-  <div class="d-flex p-4">
+  <div class="d-flex p-3">
     <div class ="col-8">INTE 22222 - Management of Technology</div>
-    <div>Some text..</div>
+    <div className="text-center col">Some text..</div>
   </div>
   
 </div>
